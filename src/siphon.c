a78913274e3f7ffc478d7b2009721585df23838d
Braxton Huggins <braxton@pagodabox.com>
2016-05-02 12:06:25 -0600

    Remove the need for libexpect, tcl, and avoid issue with long escape sequences

--- src/siphon.c
+++ src/siphon.c
@@ -1,37 +1,50 @@
+#define _XOPEN_SOURCE 
 #include <stdio.h>    // stderr, stdin, stdout - standard I/O streams
 #include <string.h>   // string operations
 #include <stdbool.h>  // boolean type and values
 #include <unistd.h>   // standard symbolic constants and types
 #include <stdlib.h>   // standard library definitions
 #include <getopt.h>   // command option parsing
-#include <tcl8.6/expect.h> // exp_spawnv
+// #include <tcl8.6/expect.h> // exp_spawnv
 // #include <tcl8.6/tcl.h> // exp_spawnv
 #include <termios.h>
+#include <signal.h>
 #include <sys/ioctl.h>
 #include <errno.h>
 #include <pty.h>
 #include <fcntl.h>
+#include <stropts.h>
 
 #define BUF_SIZE 2
 
-#ifndef TRUE
 #define TRUE 1
 #define FALSE 0
-#endif
 
 #define sysreturn(x)  return(errno = x, -1)
 
 #define EXP_MATCH_MAX 2000
 
-// int exp_ttycopy = TRUE;     /* copy tty parms from /dev/tty */
-// int exp_ttyinit = TRUE;     /* set tty parms to sane state */
-// char *exp_stty_init = 0;    /* initial stty args */
-// int exp_console = FALSE;    /* redirect console */
-// void (*exp_child_exec_prelude)() = 0;
-// void (*exp_close_in_child)() = 0;
-// static int exp_autoallocpty = 1;
-// static int exp_pty[2];
-// static pid_t exp_pid;
+#define SET_TTYTYPE 1
+
+int exp_ttycopy = TRUE;     /* copy tty parms from /dev/tty */
+int exp_ttyinit = TRUE;     /* set tty parms to sane state */
+char *exp_stty_init = 0;    /* initial stty args */
+int exp_console = FALSE;    /* redirect console */
+void (*exp_child_exec_prelude)() = 0;
+void (*exp_close_in_child)() = 0;
+static int exp_autoallocpty = 1;
+static int exp_pty[2];
+static pid_t exp_pid;
+static int knew_dev_tty;/* true if we had our hands on /dev/tty at any time */
+struct termios exp_tty_original, exp_tty_current;
+int exp_dev_tty;
+char *master_name;
+char *slave_name;
+struct winsize winsize;
+char *exp_pty_slave_name;
+
+static int locked = FALSE;
+static char lock[] = "/tmp/ptylock.XXXX"; /* XX is replaced by pty id */
 
 static unsigned int bufsiz = 2*EXP_MATCH_MAX;
 
@@ -97,7 +110,8 @@ stream(char *prefix, FILE *in)
   // for prefix width when this happens
   bool escape_seq = false;
   // create a buffer to store the current escape sequence
-  char *escape_seq_buf = calloc(16, sizeof(char));
+  char *escape_seq_buf = NULL;
+  int escape_seq_buf_size = 0;
   // store an index for the escape sequence character count
   int escape_seq_index = 0;
 
@@ -118,12 +132,24 @@ stream(char *prefix, FILE *in)
       // as soon as we see an escape character
       if (c == '\x1b') {
         escape_seq = true;
+        escape_seq_buf_size = 16;
+        escape_seq_buf = calloc(escape_seq_buf_size, sizeof(char));
         escape_seq_buf[0] = c;
         escape_seq_index++;
       } else if (escape_seq == true) {
         // add character to escape seq
         escape_seq_buf[escape_seq_index] = c;
         escape_seq_index++;
+        if (escape_seq_index == escape_seq_buf_size) {
+          char *new_escape_seq_buf = realloc(escape_seq_buf, escape_seq_buf_size * 2 * sizeof(char));
+          if (new_escape_seq_buf != NULL) {
+            memset(new_escape_seq_buf + escape_seq_buf_size, 0, escape_seq_buf_size);
+            escape_seq_buf_size = escape_seq_buf_size * 2;
+            escape_seq_buf = new_escape_seq_buf;
+          } else {
+            exit(1);
+          }
+        }
 
         // check if escape sequence is complete
         if (is_sequence_end(&c) == true) {
@@ -139,7 +165,10 @@ stream(char *prefix, FILE *in)
           // reset initialized escape sequence
           escape_seq = false;
           // clear the escape sequence buffer
-          memset(escape_seq_buf, 0, 16);
+          // memset(escape_seq_buf, 0, 16);
+          free(escape_seq_buf);
+          escape_seq_buf = NULL;
+          escape_seq_buf_size = 0;
           // reset the escape sequence buffer
           escape_seq_index = 0;
         }
@@ -258,6 +287,177 @@ main(int argc, char *argv[])
 
 }
 
+void
+exp_slave_control(master,control)
+int master;
+int control;  /* if 1, enable pty trapping of close/open/ioctl */
+{
+}
+
+void
+exp_pty_unlock(void)
+{
+  if (locked) {
+    (void) unlink(lock);
+    locked = FALSE;
+  }
+}
+
+int exp_window_size_set(fd)
+int fd;
+{
+  ioctl(fd,TIOCSWINSZ,&winsize);
+}
+
+/*static*/ struct termios exp_tty_current, exp_tty_cooked;
+#define tty_current exp_tty_current
+#define tty_cooked exp_tty_cooked
+
+void
+exp_init_tty()
+{
+  extern struct termios exp_tty_original;
+
+  /* save original user tty-setting in 'cooked', just in case user */
+  /* asks for it without earlier telling us what cooked means to them */
+  tty_cooked = exp_tty_original;
+
+  /* save our current idea of the terminal settings */
+  tty_current = exp_tty_original;
+}
+
+static void
+pty_stty(s,name)
+char *s;    /* args to stty */
+char *name;   /* name of pty */
+{
+#define MAX_ARGLIST 10240
+  char buf[MAX_ARGLIST];  /* overkill is easier */
+  void (*old)();  /* save old sigalarm handler */
+
+  sprintf(buf,"%s %s > %s","/bin/stty",s,name);
+  old = signal(SIGCHLD, SIG_DFL);
+  int ret = system(buf);
+  signal(SIGCHLD, old); /* restore signal handler */
+}
+
+#define GET_TTYTYPE 0
+#define SET_TTYTYPE 1
+static void
+ttytype(request,fd,ttycopy,ttyinit,s)
+int request;
+int fd;
+    /* following are used only if request == SET_TTYTYPE */
+int ttycopy;  /* true/false, copy from /dev/tty */
+int ttyinit;  /* if true, initialize to sane state */
+char *s;  /* stty args */
+{
+  if (request == GET_TTYTYPE) {
+    if (-1 == tcgetattr(fd, &exp_tty_original)) {
+      knew_dev_tty = FALSE;
+      exp_dev_tty = -1;
+    }
+    exp_window_size_get(fd);
+  } else {  /* type == SET_TTYTYPE */
+    if (ttycopy && knew_dev_tty) {
+      (void) tcsetattr(fd, TCSADRAIN, &exp_tty_current);
+      exp_window_size_set(fd);
+    }
+
+/* Apollo Domain doesn't need this */
+    if (ttyinit) {
+      /* overlay parms originally supplied by Makefile */
+/* As long as BSD stty insists on stdout == stderr, we can no longer write */
+/* diagnostics to parent stderr, since stderr has is now child's */
+/* Maybe someday they will fix stty? */
+/*      expDiagLogPtrStr("exp_getptyslave: (default) stty %s\n",DFLT_STTY);*/
+      pty_stty("sane",slave_name);
+    }
+
+    /* lastly, give user chance to override any terminal parms */
+    if (s) {
+      /* give user a chance to override any terminal parms */
+/*      expDiagLogPtrStr("exp_getptyslave: (user-requested) stty %s\n",s);*/
+      pty_stty(s,slave_name);
+    }
+  }
+}
+
+int
+exp_getptyslave(
+    int ttycopy,
+    int ttyinit,
+    char *stty_args)
+{
+  int slave, slave2;
+  char buf[10240];
+
+  if (0 > (slave = open(slave_name, O_RDWR))) {
+    // static char buf[500];
+    // exp_pty_error = buf;
+    // sprintf(exp_pty_error,"open(%s,rw) = %d (%s)",slave_name,slave,expErrnoMsg(errno));
+    return(-1);
+  }
+
+  if (ioctl(slave, I_PUSH, "ptem")) {
+    // expDiagLogPtrStrStr("ioctl(%d,I_PUSH,\"ptem\") = %s\n",slave,expErrnoMsg(errno));
+  }
+  if (ioctl(slave, I_PUSH, "ldterm")) {
+    // expDiagLogPtrStrStr("ioctl(%d,I_PUSH,\"ldterm\") = %s\n",slave,expErrnoMsg(errno));
+  }
+  if (ioctl(slave, I_PUSH, "ttcompat")) {
+    // expDiagLogPtrStrStr("ioctl(%d,I_PUSH,\"ttcompat\") = %s\n",slave,expErrnoMsg(errno));
+  }
+
+
+  if (0 == slave) {
+    /* if opened in a new process, slave will be 0 (and */
+    /* ultimately, 1 and 2 as well) */
+
+    /* duplicate 0 onto 1 and 2 to prepare for stty */
+    fcntl(0,F_DUPFD,1);
+    fcntl(0,F_DUPFD,2);
+  }
+
+  ttytype(SET_TTYTYPE,slave,ttycopy,ttyinit,stty_args);
+
+  (void) exp_pty_unlock();
+  return(slave);
+}
+
+int exp_window_size_get(fd)
+int fd;
+{
+  ioctl(fd,TIOCGWINSZ,&winsize);
+}
+
+int
+exp_getptymaster()
+{
+  char *hex, *bank;
+  struct stat stat_buf;
+  int master = -1;
+  int slave = -1;
+  int num;
+
+  if ((master = open("/dev/ptmx", O_RDWR)) == -1) return(-1);
+  if ((slave_name = (char *)ptsname(master)) == NULL) {
+    close(master);
+    return(-1);
+  }
+  if (grantpt(master)) {
+    close(master);
+    return(-1);
+  }
+  if (-1 == (int)unlockpt(master)) {
+    close(master);
+    return(-1);
+  }
+  exp_pty_slave_name = slave_name;
+  return(master);
+
+}
+
 static struct f *
 fd_new(fd)
 int fd;
@@ -300,7 +500,15 @@ static
 void
 exp_setpgrp()
 {
-    (void) setpgrp();
+  (void) setpgrp();
+}
+
+void
+exp_init_pty()
+{
+  exp_dev_tty = open("/dev/tty",O_RDWR);
+  knew_dev_tty = (exp_dev_tty != -1);
+  if (knew_dev_tty) ttytype(GET_TTYTYPE,exp_dev_tty,0,0,(char *)0);
 }
 
 int
@@ -310,23 +518,16 @@ bool prefix_set;
 char *file;
 char *argv[]; /* some compiler complains about **argv? */
 {
-  // int exp_autoallocpty = 1;
   int cc;
   int errorfd;  /* place to stash fileno(stderr) in child */
       /* while we're setting up new stderr */
   int ttyfd;
   struct winsize ws;
-  // pid_t exp_pid;
-  // int exp_pty[2];
   int sync_fds[2];
   int sync2_fds[2];
   int status_pipe[2];
   int child_errno;
   char sync_byte;
-#ifdef PTYTRAP_DIES
-  int slave_write_ioctls = 1;
-    /* by default, slave will be write-ioctled this many times */
-#endif
 
   static int first_time = TRUE;
 
@@ -334,7 +535,6 @@ char *argv[]; /* some compiler complains about **argv? */
     first_time = FALSE;
     exp_init_pty();
     exp_init_tty();
-    // expDiagLogPtrSet(expDiagLogU);
 
     /*
      * TIP 27; It is unclear why this code produces a
@@ -342,31 +542,14 @@ char *argv[]; /* some compiler complains about **argv? */
      * (line 512) does not generate a warning !
      */
 
-    // expErrnoMsgSet(Tcl_ErrnoMsg);
   }
 
   if (!file || !argv) sysreturn(EINVAL);
-  // if (!argv[0] || strcmp(file,argv[0])) {
-  //   exp_debuglog("expect: warning: file (%s) != argv[0] (%s)\n",
-  //     file,
-  //     argv[0]?argv[0]:"");
-  // }
-
-#ifdef PTYTRAP_DIES
-/* any extraneous ioctl's that occur in slave must be accounted for
-when trapping, see below in child half of fork */
-#if defined(TIOCSCTTY) && !defined(CIBAUD) && !defined(sun) && !defined(hp9000s300)
-  slave_write_ioctls++;
-#endif
-#endif /*PTYTRAP_DIES*/
 
   if (exp_autoallocpty) {
     if (0 > (exp_pty[0] = exp_getptymaster())) sysreturn(ENODEV);
   }
   fcntl(exp_pty[0],F_SETFD,1);  /* close on exec */
-#ifdef PTYTRAP_DIES
-  exp_slave_control(exp_pty[0],1);*/
-#endif
 
   if (!fd_new(exp_pty[0])) {
     errno = ENOMEM;
@@ -397,42 +580,13 @@ when trapping, see below in child half of fork */
     close(sync2_fds[0]);
     close(status_pipe[1]);
 
-    // if (!exp_autoallocpty) close(exp_pty[1]);
-
-#ifdef PTYTRAP_DIES
-#ifdef HAVE_PTYTRAP
-    if (exp_autoallocpty) {
-      /* trap initial ioctls in a feeble attempt to not */
-      /* block the initially.  If the process itself */
-      /* ioctls /dev/tty, such blocks will be trapped */
-      /* later during normal event processing */
-
-      while (slave_write_ioctls) {
-        int cc;
-
-        cc = exp_wait_for_slave_open(exp_pty[0]);
-#if defined(TIOCSCTTY) && !defined(CIBAUD) && !defined(sun) && !defined(hp9000s300)
-        if (cc == TIOCSCTTY) slave_write_ioctls = 0;
-#endif
-        if (cc & IOC_IN) slave_write_ioctls--;
-        else if (cc == -1) {
-          printf("failed to trap slave pty");
-          return -1;
-        }
-      }
-    }
-#endif
-#endif /*PTYTRAP_DIES*/
-
     /*
      * wait for slave to initialize pty before allowing
      * user to send to it
      */ 
 
-    // exp_debuglog("parent: waiting for sync byte\r\n");
     cc = read(sync_fds[0],&sync_byte,1);
     if (cc == -1) {
-      // exp_errorlog("parent sync byte read: %s\r\n",Tcl_ErrnoMsg(errno));
       return -1;
     }
 
@@ -443,14 +597,10 @@ when trapping, see below in child half of fork */
      * tell slave to go on now now that we have initialized pty
      */
 
-    // exp_debuglog("parent: telling child to go ahead\r\n");
     cc = write(sync2_fds[1]," ",1);
     if (cc == -1) {
-      // exp_errorlog("parent sync byte write: %s\r\n",Tcl_ErrnoMsg(errno));
       return -1;
     }
-
-    // exp_debuglog("parent: now unsynchronized from child\r\n");
 
     close(sync_fds[0]);
     close(sync2_fds[1]);
@@ -488,39 +638,18 @@ when trapping, see below in child half of fork */
   close(status_pipe[0]);
   fcntl(status_pipe[1],F_SETFD,1);  /* close on exec */
 
-#ifdef CRAY
-  (void) close(exp_pty[0]);
-#endif
 
 /* ultrix (at least 4.1-2) fails to obtain controlling tty if setsid */
 /* is called.  setpgrp works though.  */
-#if defined(POSIX) && !defined(ultrix)
-#define DO_SETSID
-#endif
-#ifdef __convex__
-#define DO_SETSID
-#endif
-
-#ifdef DO_SETSID
   setsid();
-#else
-#ifdef SYSV3
-#ifndef CRAY
-  exp_setpgrp();
-#endif /* CRAY */
-#else /* !SYSV3 */
   exp_setpgrp();
 
-#ifdef TIOCNOTTY
+
   ttyfd = open("/dev/tty", O_RDWR);
   if (ttyfd >= 0) {
     (void) ioctl(ttyfd, TIOCNOTTY, (char *)0);
     (void) close(ttyfd);
   }
-#endif /* TIOCNOTTY */
-
-#endif /* SYSV3 */
-#endif /* DO_SETSID */
 
   /* save error fd while we're setting up new one */
   errorfd = fcntl(2,F_DUPFD,3);
@@ -538,7 +667,6 @@ when trapping, see below in child half of fork */
       if (0 > (exp_pty[1] = exp_getptyslave(exp_ttycopy,exp_ttyinit,
             exp_stty_init))) {
     restore_error_fd
-    // fprintf(stderr,"open(slave pty): %s\n",Tcl_ErrnoMsg(errno));
     exit(-1);
       }
       /* sanity check */
@@ -557,22 +685,10 @@ when trapping, see below in child half of fork */
     close(exp_pty[1]);
   }
 
-
-
 /* The test for hpux may have to be more specific.  In particular, the */
 /* code should be skipped on the hp9000s300 and hp9000s720 (but there */
 /* is no documented define for the 720!) */
 
-#if defined(TIOCSCTTY) && !defined(sun) && !defined(hpux)
-  /* 4.3+BSD way to acquire controlling terminal */
-  /* according to Stevens - Adv. Prog..., p 642 */
-#ifdef __QNX__ /* posix in general */
-  if (tcsetct(0, getpid()) == -1) {
-    restore_error_fd
-    expErrorLog("failed to get controlling terminal using TIOCSCTTY");
-    exit(-1);
-  }
-#else
   (void) ioctl(0,TIOCSCTTY,(char *)0);
   /* ignore return value - on some systems, it is defined but it
    * fails and it doesn't seem to cause any problems.  Or maybe
@@ -581,99 +697,22 @@ when trapping, see below in child half of fork */
    * different linux systems (and FreeBSD 2.5), RedHat 5.2 and
    * Debian 2.0
    */
-#endif
-#endif
-
-#ifdef CRAY
-  (void) setsid();
-  (void) ioctl(0,TCSETCTTY,0);
-  (void) close(0);
-  if (open("/dev/tty", O_RDWR) < 0) {
-    restore_error_fd
-    // fprintf(stderr,"open(/dev/tty): %s\r\n",Tcl_ErrnoMsg(errno));
-    exit(-1);
-  }
-  (void) close(1);
-  (void) close(2);
-  (void) dup(0);
-  (void) dup(0);
-  setptyutmp(); /* create a utmp entry */
-
-  /* _CRAY2 code from Hal Peterson <hrp@cray.com>, Cray Research, Inc. */
-#ifdef _CRAY2
-  /*
-   * Interpose a process between expect and the spawned child to
-   * keep the slave side of the pty open to allow time for expect
-   * to read the last output.  This is a workaround for an apparent
-   * bug in the Unicos pty driver on Cray-2's under Unicos 6.0 (at
-   * least).
-   */
-  if ((pid = fork()) == -1) {
-    restore_error_fd
-    // fprintf(stderr,"second fork: %s\r\n",Tcl_ErrnoMsg(errno));
-    exit(-1);
-  }
-
-  if (pid) {
-    /* Intermediate process. */
-    int status;
-    int timeout;
-    char *t;
-
-    /* How long should we wait? */
-    timeout = exp_pty_timeout;
-
-    /* Let the spawned process run to completion. */
-    while (wait(&status) < 0 && errno == EINTR)
-      /* empty body */;
-
-    /* Wait for the pty to clear. */
-    sleep(timeout);
-
-    /* Duplicate the spawned process's status. */
-    if (WIFSIGNALED(status))
-      kill(getpid(), WTERMSIG(status));
-
-    /* The kill may not have worked, but this will. */
-    exit(WEXITSTATUS(status));
-  }
-#endif /* _CRAY2 */
-#endif /* CRAY */
 
   if (exp_console) {
-#ifdef SRIOCSREDIR
-    int fd;
-
-    if ((fd = open("/dev/console", O_RDONLY)) == -1) {
-      restore_error_fd
-      fprintf(stderr, "spawn %s: cannot open console, check permissions of /dev/console\n",argv[0]);
-      exit(-1);
-    }
-    if (ioctl(fd, SRIOCSREDIR, 0) == -1) {
-      restore_error_fd
-      fprintf(stderr, "spawn %s: cannot redirect console, check permissions of /dev/console\n",argv[0]);
-    }
-    close(fd);
-#endif
-
-#ifdef TIOCCONS
     int on = 1;
     if (ioctl(0,TIOCCONS,(char *)&on) == -1) {
       restore_error_fd
       fprintf(stderr, "spawn %s: cannot open console, check permissions of /dev/console\n",argv[0]);
       exit(-1);
     }
-#endif /* TIOCCONS */
   }
 
   /* tell parent that we are done setting up pty */
   /* The actual char sent back is irrelevant. */
 
-   // exp_debuglog("child: telling parent that pty is initialized\r\n");
   cc = write(sync_fds[1]," ",1);
   if (cc == -1) {
     restore_error_fd
-    // fprintf(stderr,"child: sync byte write: %s\r\n",Tcl_ErrnoMsg(errno));
     exit(-1);
   }
   close(sync_fds[1]);
@@ -682,12 +721,9 @@ when trapping, see below in child half of fork */
   cc = read(sync2_fds[0],&sync_byte,1);
   if (cc == -1) {
     restore_error_fd
-    // exp_errorlog("child: sync byte read: %s\r\n",Tcl_ErrnoMsg(errno));
     exit(-1);
   }
   close(sync2_fds[0]);
-
-  /* exp_debuglog("child: now unsynchronized from parent\r\n"); */
 
   /* (possibly multiple) masters are closed automatically due to */
   /* earlier fcntl(,,CLOSE_ON_EXEC); */
@@ -698,14 +734,14 @@ when trapping, see below in child half of fork */
   /* allow user to do anything else to child */
   if (exp_child_exec_prelude) (*exp_child_exec_prelude)();
 
-        if (ioctl(exp_pty[0], TIOCGWINSZ, (char *) &ws) < 0)
-          fprintf(stderr, "TIOCGWINSZ error\n");
-        if (prefix_set == true)
-          ws.ws_col -= strlen(prefix);
-        if (ioctl(exp_pty[0], TIOCSWINSZ, (char *) &ws) < 0)
-          fprintf(stderr,"TIOCSWINSZ error\n");
+  if (ioctl(exp_pty[0], TIOCGWINSZ, (char *) &ws) < 0)
+    fprintf(stderr, "TIOCGWINSZ error\n");
+  if (prefix_set == true && ws.ws_col > strlen(prefix))
+    ws.ws_col -= strlen(prefix);
+  if (ioctl(exp_pty[0], TIOCSWINSZ, (char *) &ws) < 0)
+    fprintf(stderr,"TIOCSWINSZ error\n");
 
-        (void) execvp(file,argv);
+  (void) execvp(file,argv);
 
   /* Unfortunately, by now we've closed fd's to stderr, logfile
    * and debugfile.  The only reasonable thing to do is to send
